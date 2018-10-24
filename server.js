8cc28e28a49d3a9c1e6f37e1d409098c2de36406
Chris Arnesen <chris.arnesen@gmail.com>
2018-10-24 02:07:27 -0500

    Update main

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ var port = process.env.PORT || 3000;
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.listen(port, function() {
-  console.log('Listening on port ' + port);
-});
+if (require.main === module) {
+  app.listen(port, function() {
+    console.log('Listening on port ' + port);
+  });
+}
